34d28c0f2558f67106aa844a24a6ba4d4542e020
ika4649 <aika.fukuoka@gmail.com>
2024-08-06 18:18:57 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+   
+  validates :nickname,        presence: true
+  validates :first_name,      presence: true
+  validates :last_name,       presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana,  presence: true
+  validates :birthday,        presence: true 
+  
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    # 半角英字と数字の両方が含まれているかをチェックする正規表現
+    unless password.match(/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i)
+      errors.add :password, 'は半角英数字を含めて設定してください'
+    end
+  end
 end
