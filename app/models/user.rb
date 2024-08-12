ea1cf7dce9d2b471791a093b46f43a4d041ae7ce
ika4649 <aika.fukuoka@gmail.com>
2024-08-12 17:28:30 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   
+
   validates :nickname,        presence: true
   validates :first_name,      presence: true
   validates :last_name,       presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana,  presence: true
-  validates :birthday,        presence: true 
-  
+  validates :birthday,        presence: true
+
   validate :password_complexity
 
   private
@@ -19,8 +19,8 @@ class User < ApplicationRecord
     return if password.blank?
 
     # 半角英字と数字の両方が含まれているかをチェックする正規表現
-    unless password.match(/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i)
-      errors.add :password, 'は半角英数字を含めて設定してください'
-    end
+    return if password.match(/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i)
+
+    errors.add :password, 'は半角英数字を含めて設定してください'
   end
 end
