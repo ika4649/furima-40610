f8e7368db531e1ff364e4fe1b61f68ef685a22fb
ika4649 <aika.fukuoka@gmail.com>
2024-08-18 20:59:47 +0900

    コントローラーで各メソッドの定義とログインユーザーのみがアクセスできるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
-    # @items = Item.all
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
