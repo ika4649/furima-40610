e1c099d5ea8b570b8633e4d126b86426b8c96899
ika4649 <aika.fukuoka@gmail.com>
2024-08-18 20:08:09 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
   def index
     # @items = Item.all
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
