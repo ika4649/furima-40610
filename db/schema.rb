3f3ff8b078672787fa292f6d54331523a32327db
ika4649 <aika.fukuoka@gmail.com>
2024-08-24 18:59:10 +0900

    不要なマイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_20_092957) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_18_101228) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_20_092957) do
     t.string "name", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
+    t.integer "condition_id", null: false
     t.integer "shipping_fee_burden_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "shipping_day_id", null: false
@@ -50,7 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_20_092957) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "condition_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
