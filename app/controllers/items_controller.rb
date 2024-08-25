062daa43584d2d060873edaafd024a3a39809ba2
ika4649 <aika.fukuoka@gmail.com>
2024-08-25 14:21:51 +0900

    Rubcopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -17,9 +17,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def show
-    #@item = Item.find(params[:id])
-  #end
+  # def show
+  # @item = Item.find(params[:id])
+  # end
 
   private
 
