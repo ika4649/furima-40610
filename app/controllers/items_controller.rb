ae17db888072faf73d7aa82e29daabf0698a0027
ika4649 <aika.fukuoka@gmail.com>
2024-08-30 15:27:48 +0900

    Rubcopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
-  
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -20,19 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
-    if current_user.id != @item.user_id
-      redirect_to root_path
-    end
+    return unless current_user.id != @item.user_id
+
+    redirect_to root_path
   end
 
   def update
     if @item.update(item_params)
       redirect_to item_path(@item)
-    else 
+    else
       render :edit, status: :unprocessable_entity
     end
   end
