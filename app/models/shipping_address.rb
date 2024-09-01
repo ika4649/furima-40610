f9a4ce08d248ac6ca14bbea48685f5ae1650e671
ika4649 <aika.fukuoka@gmail.com>
2024-09-01 17:50:27 +0900

    購入記録と発送先情報のモデル作成、バリデーションとアソシエーションの設定

---
+++ app/models/shipping_address.rb
@@ -0,0 +1,11 @@
+class ShippingAddress < ApplicationRecord
+  #アソシエーションの追加
+  belongs_to :purchase_record
+
+  #バリデーションの追加
+  validates :postal_code,    presence:true, format: { with: /\A\d{3}-\d{4}\z/, message: "must be in the format 123-4567" }
+  validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank"}
+  validates :city,           presence:true
+  validates :address,        presence:true
+  validates :phone_number,   presence:true, format: { with: /\A\d{10,11}\z/, message: "must be 10 or 11 digits without hyphens" }
+end
