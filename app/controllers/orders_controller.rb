deb4e63e6f3f1fc6751ae2e4a3c3715054a7b144
ika4649 <aika.fukuoka@gmail.com>
2024-09-04 22:54:19 +0900

    フォームの情報保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
     @order_form = OrderForm.new
   end
 
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.save
+      redirect_to root_path  
+    else 
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
