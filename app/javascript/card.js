8fffc62d8866aa2cd93e303bed253a6f49db3429
ika4649 <aika.fukuoka@gmail.com>
2024-09-07 15:36:59 +0900

    ①ブラウザのカード情報を取得する

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,16 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const form = document.getElementById('payment-form');
+  const cardNumber = document.getElementById('card-number');
+  const cardExpiry = document.getElementById('card-expiry');
+  const cardCVC = document.getElementById('card-cvc');
+
+  form.addEventListener("submit", (e) => {
+    console.log("カード番号: ", cardNumber.value);  // カード番号
+    console.log("有効期限: ", cardExpiry.value);  // 有効期限
+    console.log("セキュリティコード: ", cardCVC.value);  // セキュリティコード
+    console.log("フォーム送信時にイベント発火");
+    e.preventDefault();
+  });
 };
 
 window.addEventListener("turbo:load", pay);
--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,10 +1,14 @@
 document.addEventListener('turbo:load', priceCalculation);
-document.addEventListener('turbo:render',priceCalculation);
+document.addEventListener('turbo:render', priceCalculation);
 
-function priceCalculation(){
+function priceCalculation() {
   // 価格入力フィールドの要素を取得
   const priceInput = document.getElementById('item-price');
-  
+
+  if (!priceInput) {
+    return; // priceInputが存在しない場合はここで処理を終了
+  }
+
   // 手数料と利益を表示する要素を取得
   const addTaxDom = document.getElementById('add-tax-price');
   const profitDom = document.getElementById('profit');
@@ -16,7 +20,7 @@ function priceCalculation(){
 
     // 販売手数料の計算 (10%と仮定)
     const tax = Math.floor(price * 0.1);
-    
+
     // 利益の計算
     const profit = Math.floor(price - tax);
 
