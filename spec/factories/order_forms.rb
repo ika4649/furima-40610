67fe623b34e58b60dc383c9aa03da3283641700f
ika4649 <aika.fukuoka@gmail.com>
2024-09-09 20:42:38 +0900

    修正依頼の記述２

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_form do
-    association :user
-    association :item
     postal_code { Faker::Number.between(from: 100, to: 999).to_s + '-' + Faker::Number.between(from: 1000, to: 9999).to_s }
     prefecture_id { Faker::Number.between(from: 2, to: 47) } # 日本の都道府県ID（1は除外）
     city { Faker::Address.city }
